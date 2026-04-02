d425cb97ccf283eaf3684c95b586e9465eb2d884
gdac <gabrieldalcs@gmail.com>
2026-04-02 18:22:34 -0300

    change structure of malicious request

--- test/globecom/get_harmbench_results.py
+++ test/globecom/get_harmbench_results.py
@@ -8,7 +8,7 @@ def get_harmbench_results(paths):
     generations_paths = []
     results_paths = []
     for path in paths:
-        generations_file = [f for f in os.listdir(path) if f.endswith('.json')][0]
+        generations_file = [f for f in os.listdir(path) if f.endswith('.json') and f != "result.json"][0]
         generations_paths.append(os.path.join(path, generations_file))
         results_paths.append(os.path.join(path, "result.json"))
 
